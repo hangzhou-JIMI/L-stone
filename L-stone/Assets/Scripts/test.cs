3a2be9bdf9588a5307e75eb9cd9c27399e6e26e2
luzexi <jesse_luzexi@163.com>
2014-07-15 20:09:42 +0800

    init
    init

---
+++ L-stone/Assets/Scripts/test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		//
+		if(GUI.Button(new Rect(0,0,100,30) , "test"))
+		{
+			//
+			Debug.Log("ok");
+		}
+	}
+}
