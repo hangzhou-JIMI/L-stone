291bafceb8b121c170cc3759d9ee24252e37954e
luzexi <jesse_luzexi@163.com>
2014-08-12 18:21:35 +0800

    提交
    提交

--- L-stone/Assets/Scripts/App/CApplication.cs
+++ L-stone/Assets/Scripts/App/CApplication.cs
@@ -33,7 +33,9 @@ public class CApplication : MonoBehaviour
 	{
 		if(pause)
 		{
-			PushMsg.sInstance.PushLocalMsg(DateTime.Now.AddDays(1),"萌呆,你的小伙伴已经"+GameData.s_iLevel+"层了，快来继续攀登高峰");
+			NotificationServices.CancelAllLocalNotifications();
+			int randLevel = GameData.s_iLevel + UnityEngine.Random.Range(1,20);
+			PushMsg.sInstance.PushLocalMsg(DateTime.Now.AddDays(1),"萌呆,你的小伙伴已经"+randLevel+"层了，快来继续攀登高峰");
 			PushMsg.sInstance.PushLocalMsg(DateTime.Now.AddDays(3),"Hello,你在干嘛呢?");
 			PushMsg.sInstance.PushLocalMsg(DateTime.Now.AddDays(7),"萌呆,"+GameData.s_iLevel+"层是你的极限了吗?");
 			PushMsg.sInstance.PushLocalMsg(DateTime.Now.AddMonths(1),"我被你遗弃了 T_T");
